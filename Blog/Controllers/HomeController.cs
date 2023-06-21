13bc27bd8cf7b63ef6ef7222f67a0907ef09629d
farleybsd <farley.t.i@hotmail.com>
2023-06-21 19:04:40 -0300

    Home Controller

---
+++ Blog/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("")]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                versao= "0001"
+            });
+        }
+    }
+}
