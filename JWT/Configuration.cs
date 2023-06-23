c12e0fa472b4082b11f0c75ca1079b995e9d6942
farleybsd <farley.t.i@hotmail.com>
2023-06-23 19:04:39 -0300

    Atributo

--- JWT/Configuration.cs
+++ JWT/Configuration.cs
@@ -4,5 +4,7 @@
     {
         // Token - Jwt
         public static string JwtKey { get; set; } = "721979D0-C507-45E1-A145-3285247E3FBD";
+        public static string ApiKeyName = "api_key";
+        public static string ApiKey = "curso_api_IlTevUM/z0ey3NwCV/unWg==";
     }
 }
