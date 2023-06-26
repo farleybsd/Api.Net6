802eafa94f07be60a8935f4f4f78df87432bb313
farleybsd <farley.t.i@hotmail.com>
2023-06-26 16:51:38 -0300

    Configuracao

---
+++ Perfomace/Perfomace/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Blog.Atribute;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet("")]
+        //[ApiKey] Criado um Atributo
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                versao= "0001"
+            });
+        }
+    }
+}
